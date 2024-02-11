2f9a3c8ea2a901f9aa0a5d026e27dd7d97f33c36
Your Name <you@example.com>
2024-02-12 02:25:22 +0700

    👋 Freeyaauerincatherine Bot App Apis Example

---
+++ package/freeyaauerincatherine_bot_app_apis/example/freeyaauerincatherine_bot_app_apis_example.dart
@@ -0,0 +1,6 @@
+import 'package:freeyaauerincatherine_bot_app_apis/freeyaauerincatherine_bot_app_apis.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
