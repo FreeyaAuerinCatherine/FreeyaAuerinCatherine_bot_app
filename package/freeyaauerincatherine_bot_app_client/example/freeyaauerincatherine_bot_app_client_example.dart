8f738538ebc24e013a970d741bc3875f33f2426b
Your Name <you@example.com>
2024-02-12 02:24:34 +0700

    🍡 Freeyaauerincatherine Bot App Client Example

---
+++ package/freeyaauerincatherine_bot_app_client/example/freeyaauerincatherine_bot_app_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:freeyaauerincatherine_bot_app_client/freeyaauerincatherine_bot_app_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
