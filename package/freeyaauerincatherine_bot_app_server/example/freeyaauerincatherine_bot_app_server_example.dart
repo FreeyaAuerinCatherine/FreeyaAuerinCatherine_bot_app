3a73159a93d33bbfd92db19d5470e3cd3e0dfc9d
Your Name <you@example.com>
2024-02-12 02:21:44 +0700

    📋 Freeyaauerincatherine Bot App Server Example

---
+++ package/freeyaauerincatherine_bot_app_server/example/freeyaauerincatherine_bot_app_server_example.dart
@@ -0,0 +1,6 @@
+import 'package:freeyaauerincatherine_bot_app_server/freeyaauerincatherine_bot_app_server.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
